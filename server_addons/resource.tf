370da2dc6ff2cbbbb32109782751712de11f5124
EC2 Default User <ec2-user@ip-172-31-37-165.us-west-2.compute.internal>
2018-06-23 14:20:10 +0000

    stuff2

--- server_addons/resource.tf
+++ server_addons/resource.tf
@@ -3,11 +3,18 @@ provider "aws" {
 }
 
 resource "aws_s3_bucket" "s3_bucket_created_via_tf" {
-	bucket = "s3_bucket_created_via_tf"
+	bucket = "s3-bucket-created-via-tf"
 	acl = "private"
 
 	tags {
 		Name = "My Bucket"
 		Environment = "SBX"
              }
+}
+resource "aws_efs_file_system" "my_efs" {
+  creation_token = "myefs"
+
+  tags {
+    Name = "Shared between all servers"
+  }
 }
