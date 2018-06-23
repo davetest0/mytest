165e07bec0e9a2f00d2e8971a247ac4f14d4f2fa
EC2 Default User <ec2-user@ip-172-31-37-165.us-west-2.compute.internal>
2018-06-23 02:15:17 +0000

    added resource and provider for creating s3 bucket

---
+++ resource.tf
@@ -0,0 +1,9 @@
+resource "aws_s3_bucket" "s3_bucket_created_via_tf" {
+	bucket = "s3_bucket_created_via_tf"
+	acl = "private"
+
+	tags {
+		Name = "My Bucket"
+		Environment = "SBX"
+             }
+}
