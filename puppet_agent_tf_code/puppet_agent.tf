5bbe9d04fa6d316ec9b0cf6652b1ac71c3083d05
davetest0 <d_moeller@hotmail.com>
2018-06-23 14:27:56 +0000

    stuff

---
+++ puppet_agent_tf_code/puppet_agent.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region     = "us-west-2"
+}
+
+resource "aws_instance" "puppetagent" {
+  ami           = "ami-3ecc8f46"
+  instance_type = "t2.micro"
+  key_name = "mypersonalec2"
+tags {
+	"Creation_Order" = "3"
+	"Server_role"    = "PuppetAgent"
+}
+}
+
