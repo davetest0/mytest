0d3c52852887da5bd095dfda688ce02de5798347
EC2 Default User <ec2-user@ip-172-31-37-165.us-west-2.compute.internal>
2018-06-26 16:07:07 +0000

    added some provisioner code to echo ip address

--- puppet_agent_tf_code/puppet_agent.tf
+++ puppet_agent_tf_code/puppet_agent.tf
@@ -6,9 +6,14 @@ resource "aws_instance" "puppetagent" {
   ami           = "ami-3ecc8f46"
   instance_type = "t2.micro"
   key_name = "mypersonalec2"
+  provisioner "local-exec" {
+     command = "echo ${aws_instance.puppetagent.public_ip} >> $aws_instance_ip_address.txt
+  }
+
+
 tags {
 	"Creation_Order" = "3"
 	"Server_role"    = "PuppetAgent"
-}
+     }
 }
 
